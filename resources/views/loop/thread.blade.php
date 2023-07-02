a1aab012b7090bdb8ecc091609b9b1129275de76
Klongchu Dev <klongchu@gmail.com>
2023-07-02 23:00:53 +0700

    bug

--- resources/views/loop/thread.blade.php
+++ resources/views/loop/thread.blade.php
@@ -13,7 +13,7 @@
 	<span class="d-inline-block mr-5">{{ $thread->user->username }}</span>
 	<span class="badge badge-danger">{{ $thread->messages->count() }}</span>
 	<span>{{ $thread->subject }}</span>
-	<small class="text-muted">- {{ str_limit($thread->lastMessage()->body, 50) }}</small>
+	<small class="text-muted">- {{ \Illuminate\Support\Str::limit($thread->lastMessage()->body, 50) }}</small>
 	<span class="float-right badge badge-secondary ml-2">{{ $thread->updated_at->diffForHumans() }}</span>
 	{{--<a href="{{ route('inbox.destroy', $thread->id) }}"><span class="fa fa-trash"></span></a>--}}
 	{{--<span class="float-right fa fa-paperclip"></span>--}}
